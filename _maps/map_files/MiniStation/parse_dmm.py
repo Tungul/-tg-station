d5eeab0710cd5d7a7996b8403fb8f5bc8c9d27cf
Lugnut <lugnut1206>
2014-03-07 01:18:56 -0500

    stock setup

---
+++ _maps/map_files/MiniStation/parse_dmm.py
@@ -0,0 +1,22 @@
+""" Parses maps and fixes step_ error. """
+
+import urllib.request
+import sys
+
+to_parse = str(sys.argv[1])
+print("Downloading %s" % to_parse)
+
+# Download file, parse, then store.
+
+request = urllib.request.urlopen(to_parse)
+
+print("Parsing file.")
+
+file_data = request.read().decode('utf-8')
+file_data = file_data.replace("step_", "pixel_")
+
+print("Writing file.")
+# Store file
+
+with open("MiniStation.dmm", mode='w') as output_file:
+    output_file.write(file_data)
